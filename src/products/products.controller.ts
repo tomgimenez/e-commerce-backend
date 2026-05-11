f0ab8aa124788f7d28bf56f8f53430828080af55
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-11 19:49:26 -0300

    remove console.log

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -41,7 +41,6 @@ export class ProductsController {
 
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
-    // console.log(paginationDto)
     return this.productsService.findAll(paginationDto);
   }
 
