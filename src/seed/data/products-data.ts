32e4c4bc2fa4c153e3e742659940a19f65ee391a
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-17 14:58:51 -0300

    separate seed file

---
+++ src/seed/data/products-data.ts
@@ -0,0 +1,599 @@
+export const productsData = [
+  {
+    "title": "The Fellowship of the Ring",
+    "description": "The first part of the epic masterpiece The Lord of the Rings, following Frodo Baggins as he begins his perilous journey to destroy the One Ring.",
+    "images": [
+      "fellowship-of-the-ring.jpg"
+    ],
+    "stock": 120,
+    "price": 12.00,
+    "tags": ["High Fantasy", "Epic Fantasy", "Classic"],
+    "rating": 4.9,
+    "reviews": 85000,
+    
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "Epic Fantasy" }
+    ],
+    "attributes": {
+      "author": "J.R.R. Tolkien",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Harry Potter and the Philosopher's Stone",
+    "description": "A young boy discovers he is a wizard and attends Hogwarts School of Witchcraft and Wizardry, changing the world of magic forever.",
+    "images": ["harry-potter-philosophers-stone.jpg",],
+    "stock": 200,
+    "price": 19.99,
+    "tags": ["Low Fantasy", "Magic School", "Young Adult"],
+    "rating": 4.8,
+    "reviews": 150000,
+    "categories": [
+      { "name": "Low Fantasy" },
+      { "name": "Magical Realism" }
+    ],
+    "attributes": {
+      "author": "J.K. Rowling",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Hobbit",
+    "description": "Bilbo Baggins, a home-loving hobbit, is whisked away on an adventure by a wizard and a company of dwarves to reclaim a lost treasure.",
+    "images": ["the-hobbit.jpg"],
+    "stock": 85,
+    "price": 9.50,
+    "tags": ["High Fantasy", "Adventure", "Classic"],
+    "rating": 4.8,
+    "reviews": 62000,
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "Heroic Fantasy" }
+    ],
+    "attributes": {
+      "author": "J.R.R. Tolkien",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Name of the Wind",
+    "description": "The tale of Kvothe, a legendary figure who tells his own story of magic, music, and mystery.",
+    "images": ["the-name-of-the-wind.jpg"],
+    "stock": 12,
+    "price": 18.50,
+    "tags": ["High Fantasy", "Magic School"],
+    "rating": 4.8,
+    "reviews": 25000,
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "Magical Realism" }
+    ],
+    "attributes": {
+      "author": "Patrick Rothfuss",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Way of Kings",
+    "description": "The first book in The Stormlight Archive, an epic masterpiece set on the world of Roshar, a world of stone and storms.",
+    "images": ["the-way-of-kings.jpg"],
+    "stock": 45,
+    "price": 29.99,
+    "tags": ["Epic Fantasy", "High Fantasy", "Magic Systems"],
+    "rating": 4.9,
+    "reviews": 12500,
+    "categories": [
+      { "name": "Epic Fantasy" },
+      { "name": "High Fantasy" }
+    ],
+    "attributes": {
+      "author": "Brandon Sanderson",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Blade Itself",
+    "description": "A gritty tale where the line between hero and villain is non-existent. The first book of The First Law trilogy.",
+    "images": ["the-blade-itself.jpg"],
+    "stock": 30,
+    "price": 15.99,
+    "tags": ["Grimdark", "Low Fantasy", "Action"],
+    "rating": 4.5,
+    "reviews": 8900,
+    "categories": [
+      { "name": "Grimdark" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Joe Abercrombie",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "A Court of Thorns and Roses",
+    "description": "A blend of romance and faerie lore where a young huntress is captured by a lethal, immortal faerie.",
+    "images": ["a-court-of-thrones-and-roses.jpg"],
+    "stock": 150,
+    "price": 8.99,
+    "tags": ["Romantic Fantasy", "Fae", "Romance"],
+    "rating": 4.7,
+    "reviews": 45000,
+    "categories": [
+      { "name": "Romantic Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Sarah J. Maas",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Storm Front",
+    "description": "Harry Dresden is a wizard for hire in modern-day Chicago, dealing with supernatural crimes the police can't explain.",
+    "images": ["storm-front.jpg"],
+    "stock": 25,
+    "price": 12.99,
+    "tags": ["Urban Fantasy", "Mystery", "Magic"],
+    "rating": 4.3,
+    "reviews": 5600,
+    "categories": [
+      { "name": "Urban Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Jim Butcher",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "The Lies of Locke Lamora",
+    "description": "In the city of Camorr, a group of elite con artists known as the Gentleman Bastards plot their biggest heist.",
+    "images": ["the-lies-of-locke-lamora.jpg"],
+    "stock": 18,
+    "price": 16.50,
+    "tags": ["Heroic Fantasy", "Heist", "Comedy"],
+    "rating": 4.6,
+    "reviews": 9200,
+    "categories": [
+      { "name": "Heroic Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Scott Lynch",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "Gardens of the Moon",
+    "description": "The sprawling opening to the Malazan Book of the Fallen, featuring gods, mages, and elder races.",
+    "images": ["gardens-of-the-moon.jpg"],
+    "stock": 8,
+    "price": 19.99,
+    "tags": ["Epic Fantasy", "High Fantasy", "Complex"],
+    "rating": 4.2,
+    "reviews": 4100,
+    "categories": [
+      { "name": "Epic Fantasy" },
+      { "name": "High Fantasy" }
+    ],
+    "attributes": {
+      "author": "Steven Erikson",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "Circe",
+    "description": "A bold reimagining of the Greek myth, following the banished witch Circe as she tames wild beasts and crosses paths with gods.",
+    "images": ["circe.jpg"],
+    "stock": 80,
+    "price": 17.00,
+    "tags": ["Historical Fantasy", "Mythology", "Literary"],
+    "rating": 4.8,
+    "reviews": 32000,
+    "categories": [
+      { "name": "Historical Fantasy" },
+      { "name": "Magical Realism" }
+    ],
+    "attributes": {
+      "author": "Madeline Miller",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Eye of the World",
+    "description": "The journey begins for five young villagers as they are thrust into a world of prophecy and danger.",
+    "images": ["the-eye-of-the-world.jpg"],
+    "stock": 60,
+    "price": 25.00,
+    "tags": ["High Fantasy", "Epic Fantasy", "Prophecy"],
+    "rating": 4.7,
+    "reviews": 18000,
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "Epic Fantasy" }
+    ],
+    "attributes": {
+      "author": "Robert Jordan",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "American Gods",
+    "description": "Old gods and new gods battle for the soul of America in this dark, surreal road trip.",
+    "images": ["american-gods.jpg"],
+    "stock": 40,
+    "price": 14.99,
+    "tags": ["Urban Fantasy", "Low Fantasy", "Mythology"],
+    "rating": 4.4,
+    "reviews": 11000,
+    "categories": [
+      { "name": "Urban Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Neil Gaimann",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "A Game of Thrones",
+    "description": "Kings and queens, knights and renegades, liars and honest men play a deadly game for control of the Seven Kingdoms.",
+    "images": ["a-game-of-thrones.jpg"],
+    "stock": 55,
+    "price": 22.50,
+    "tags": ["Epic Fantasy", "Grimdark", "Political"],
+    "rating": 4.8,
+    "reviews": 95000,
+    "categories": [
+      { "name": "Grimdark" },
+      { "name": "Epic Fantasy" }
+    ],
+    "attributes": {
+      "author": "George R.R. Martin",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Last Wish",
+    "description": "Geralt of Rivia is a Witcher, a man whose magic powers, enhanced by long training and a mysterious elixir, have made him a brilliant fighter and a merciless assassin.",
+    "images": ["the-last-wish.jpg"],
+    "stock": 40,
+    "price": 16.99,
+    "tags": ["Heroic Fantasy", "Monsters", "Adventure"],
+    "rating": 4.7,
+    "reviews": 32000,
+    "categories": [
+      { "name": "Heroic Fantasy" },
+      { "name": "High Fantasy" }
+    ],
+    "attributes": {
+      "author": "Andrzej Sapkowski",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Assassin's Apprentice",
+    "description": "Young Fitz is the bastard son of a noble prince, trained in the art of the assassin while struggling with his magical link to animals.",
+    "images": ["assassins-apprentice.jpg"],
+    "stock": 25,
+    "price": 18.00,
+    "tags": ["High Fantasy", "Magic", "Coming of Age"],
+    "rating": 4.6,
+    "reviews": 15000,
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Robin Hobb",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Mistborn: The Final Empire",
+    "description": "In a world where ash falls from the sky, a street urchin discovers she has the power of an Allomancer.",
+    "images": ["mistborn-the-final-empire.jpg"],
+    "stock": 65,
+    "price": 21.00,
+    "tags": ["High Fantasy", "Magic Systems", "Heist"],
+    "rating": 4.8,
+    "reviews": 42000,
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "LitRPG" }
+    ],
+    "attributes": {
+      "author": "Brandon Sanderson",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Good Omens",
+    "description": "An angel and a demon who have lived on Earth since the Beginning must team up to stop the approaching Apocalypse.",
+    "images": ["good-omens.jpg"],
+    "stock": 30,
+    "price": 14.99,
+    "tags": ["Low Fantasy", "Comedy", "Urban Fantasy"],
+    "rating": 4.7,
+    "reviews": 28000,
+    "categories": [
+      { "name": "Urban Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Neil Gaiman & Terry Pratchett",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Poppy War",
+    "description": "A dark historical fantasy inspired by 20th-century China, following a war orphan who enters a prestigious military academy.",
+    "images": ["the-poppy-war.jpg"],
+    "stock": 20,
+    "price": 19.50,
+    "tags": ["Grimdark", "Historical Fantasy", "Magic"],
+    "rating": 4.5,
+    "reviews": 12000,
+    "categories": [
+      { "name": "Grimdark" },
+      { "name": "Historical Fantasy" }
+    ],
+    "attributes": {
+      "author": "R.F. Kuang",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "The Color of Magic",
+    "description": "The first book of the Discworld series, following the wizard Rincewind and the world's first tourist.",
+    "images": ["the-color-of-magic.jpg"],
+    "stock": 45,
+    "price": 12.00,
+    "tags": ["Comedy", "High Fantasy", "Satire"],
+    "rating": 4.4,
+    "reviews": 21000,
+    "categories": [
+      { "name": "Low Fantasy" },
+      { "name": "Magical Realism" }
+    ],
+    "attributes": {
+      "author": "Terry Pratchett",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Ninth House",
+    "description": "Alex Stern can see ghosts, a power that lands her a spot at Yale to monitor the school's occult secret societies.",
+    "images": ["the-ninth-house.jpg"],
+    "stock": 35,
+    "price": 24.00,
+    "tags": ["Urban Fantasy", "Dark Academia", "Mystery"],
+    "rating": 4.3,
+    "reviews": 18000,
+    "categories": [
+      { "name": "Urban Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Leigh Bardugo",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Priory of the Orange Tree",
+    "description": "A sprawling epic featuring dragon riders, queens, and an ancient evil threatening to return.",
+    "images": ["the-priory-of-the-orange-tree.jpg"],
+    "stock": 15,
+    "price": 32.00,
+    "tags": ["Epic Fantasy", "Dragons", "High Fantasy"],
+    "rating": 4.6,
+    "reviews": 9500,
+    "categories": [
+      { "name": "Epic Fantasy" },
+      { "name": "High Fantasy" }
+    ],
+    "attributes": {
+      "author": "Samantha Shannon",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "Fourth Wing",
+    "description": "Violet Sorrengail must survive a brutal elite college for dragon riders where the competition is literally deadly.",
+    "images": ["fourth-wing.jpg"],
+    "stock": 90,
+    "price": 26.99,
+    "tags": ["Romantic Fantasy", "Dragons", "New Adult"],
+    "rating": 4.8,
+    "reviews": 110000,
+    "categories": [
+      { "name": "Romantic Fantasy" },
+      { "name": "High Fantasy" }
+    ],
+    "attributes": {
+        "author": "Rebecca Yarros",
+        "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Black Company",
+    "description": "Follows a group of elite mercenaries through a dark and gritty world serving a powerful sorceress.",
+    "images": ["the-black-company.jpg"],
+    "stock": 12,
+    "price": 15.00,
+    "tags": ["Grimdark", "Military Fantasy"],
+    "rating": 4.5,
+    "reviews": 7800,
+    "categories": [
+      { "name": "Grimdark" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Glen Cook",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "Ocean at the End of the Lane",
+    "description": "A man returns to his childhood home and remembers a series of supernatural events from his past.",
+    "images": ["ocean-at-the-end-of-the-lane.jpg"],
+    "stock": 25,
+    "price": 16.50,
+    "tags": ["Low Fantasy", "Magical Realism", "Literary"],
+    "rating": 4.6,
+    "reviews": 19000,
+    "categories": [
+      { "name": "Low Fantasy" },
+      { "name": "Magical Realism" }
+    ],
+    "attributes": {
+      "author": "Neil Gaiman",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Eragon",
+    "description": "A farm boy discovers a polished blue stone that hatches into a dragon, thrusting him into a world of magic and power.",
+    "images": ["eragon.jpg"],
+    "stock": 50,
+    "price": 14.00,
+    "tags": ["High Fantasy", "Dragons", "YA"],
+    "rating": 4.4,
+    "reviews": 55000,
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "Heroic Fantasy" }
+    ],
+    "attributes": {
+      "author": "Christopher Paolini",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "The Lion, the Witch and the Wardrobe",
+    "description": "Four siblings step through a wardrobe into the magical land of Narnia, frozen in eternal winter.",
+    "images": ["the-lion-the-witch-wardrobe.jpg"],
+    "stock": 110,
+    "price": 12.99,
+    "tags": ["Portal Fantasy", "Classic", "Children's"],
+    "rating": 4.8,
+    "reviews": 75000,
+    "categories": [
+      { "name": "Portal Fantasy" },
+      { "name": "Magical Realism" }
+    ],
+    "attributes": {
+      "author": "C.S. Lewis",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Uprooted",
+    "description": "A young woman is chosen by a wizard known as the Dragon to serve in his tower, but the real threat is the Wood.",
+    "images": ["uprooted.jpg"],
+    "stock": 18,
+    "price": 17.99,
+    "tags": ["Fairy Tale", "High Fantasy", "Magic"],
+    "rating": 4.6,
+    "reviews": 14000,
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "Magical Realism" }
+    ],
+    "attributes": {
+      "author": "Naomi Novik",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "Daughter of the Moon Goddess",
+    "description": "A young woman's quest to free her mother leads her into a world of immortals and ancient Chinese legends.",
+    "images": ["daughter-of-the-moon-goddess.jpg"],
+    "stock": 22,
+    "price": 20.00,
+    "tags": ["Xianxia", "Mythology", "Romantic Fantasy"],
+    "rating": 4.5,
+    "reviews": 9000,
+    "categories": [
+      { "name": "Wuxia / Xianxia" },
+      { "name": "Romantic Fantasy" }
+    ],
+    "attributes": {
+      "author": "Sue Lynn Tan",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Jonathan Strange & Mr Norrell",
+    "description": "In 19th-century England, two very different magicians emerge to bring magic back to the world.",
+    "images": ["jonathan-strange-mr-norrell.jpg"],
+    "stock": 10,
+    "price": 28.00,
+    "tags": ["Historical Fantasy", "Gaslamp Fantasy", "Literary"],
+    "rating": 4.4,
+    "reviews": 8500,
+    "categories": [
+      { "name": "Historical Fantasy" },
+      { "name": "Gaslamp Fantasy" }
+    ],
+    "attributes": {
+      "author": "Susanna Clarke",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Neverwhere",
+    "description": "An ordinary man falls through the cracks of reality into London Below, a world of monsters and saints.",
+    "images": ["neverwhere.jpg"],
+    "stock": 35,
+    "price": 15.50,
+    "tags": ["Urban Fantasy", "Adventure", "Low Fantasy"],
+    "rating": 4.7,
+    "reviews": 22000,
+    "categories": [
+      { "name": "Urban Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Neil Gaiman",
+      "isBestseller": true,
+    }
+  },
+  {
+    "title": "Prince of Thorns",
+    "description": "Jorg Ancrath, a young prince leading a band of bloodthirsty thugs, seeks revenge in a broken world.",
+    "images": ["prince-of-thorns.jpg"],
+    "stock": 14,
+    "price": 16.00,
+    "tags": ["Grimdark", "Dark Fantasy", "Anti-hero"],
+    "rating": 4.2,
+    "reviews": 11000,
+    "categories": [
+      { "name": "Grimdark" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Mark Lawrence",
+      "isBestseller": false,
+    }
+  },
+  {
+    "title": "Red Sister",
+    "description": "At the Convent of Sweet Mercy, young girls are trained to be killers under the dying sun of a frozen world.",
+    "images": ["red-sister.jpg"],
+    "stock": 20,
+    "price": 18.50,
+    "tags": ["High Fantasy", "Magic School", "Action"],
+    "rating": 4.6,
+    "reviews": 13000,
+    "categories": [
+      { "name": "High Fantasy" },
+      { "name": "Low Fantasy" }
+    ],
+    "attributes": {
+      "author": "Mark Lawrence",
+      "isBestseller": false,
+    }
+  }
+]
