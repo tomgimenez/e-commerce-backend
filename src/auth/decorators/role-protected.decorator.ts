fffad911924b7658b4101710b2e0ed2f5bdc7680
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-27 21:33:42 -0300

    update import

--- src/auth/decorators/role-protected.decorator.ts
+++ src/auth/decorators/role-protected.decorator.ts
@@ -1,10 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
-import { ValidRoles } from '../interfaces';
+import { ValidRoles } from 'src/user/enums/valid-roles';
 
 export const META_ROLES = 'roles';
 
 export const RoleProtected = (...roles: ValidRoles[] ) => {
-
-
     return SetMetadata( META_ROLES , roles);
 }
