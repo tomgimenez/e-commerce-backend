65b188ab614e22e1fc428cf7b29bc8f19d13becd
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-27 21:36:36 -0300

    update imports, re organize code. using user service instead of user repo

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,43 +1,36 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { Repository } from 'typeorm';
-import { User } from '../entities/user.entity';
+
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { User } from '../../user/entities/user.entity';
+import { UserService } from '../../user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy( Strategy ) {
 
-    constructor(
-        @InjectRepository( User )
-        private readonly userRepository: Repository<User>,
+  constructor(
+    private readonly userService: UserService,
+    configService: ConfigService
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
 
-        configService: ConfigService
-    ) {
+  async validate( payload: JwtPayload ): Promise<User> {
+    const { id } = payload;
+    const user = await this.userService.findById(id);
 
-        super({
-            secretOrKey: configService.get('JWT_SECRET'),
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        });
-    }
-
-
-    async validate( payload: JwtPayload ): Promise<User> {
+    if ( !user ) 
+      throw new UnauthorizedException('Token not valid')
         
-        const { id } = payload;
+    if ( !user.isActive ) 
+      throw new UnauthorizedException('User is inactive, talk with an admin');
 
-        const user = await this.userRepository.findOneBy({ id });
-
-        if ( !user ) 
-            throw new UnauthorizedException('Token not valid')
-            
-        if ( !user.isActive ) 
-            throw new UnauthorizedException('User is inactive, talk with an admin');
-        
-
-        return user;
-    }
+    return user;
+  }
 
 }
