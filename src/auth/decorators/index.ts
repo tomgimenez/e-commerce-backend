a827e5c376e3ab183b50246979086b29ca84dae0
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-27 21:32:47 -0300

    remove unused import

--- src/auth/decorators/index.ts
+++ src/auth/decorators/index.ts
@@ -1,7 +1,3 @@
 export { Auth } from './auth.decorator';
 export { RoleProtected } from './role-protected.decorator';
-
 export { RawHeaders } from './raw-headers.decorator';
-export { GetUser } from "./get-user.decorator";
-
-
