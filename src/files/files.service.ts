cd79c2bd2148a0f79776bc784234bff29a169623
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-29 12:30:24 -0300

    change identation

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,19 +3,16 @@ import { join } from 'path';
 
 import { Injectable, BadRequestException } from '@nestjs/common';
 
-
 @Injectable()
 export class FilesService {
   
-    getStaticProductImage( imageName: string ) {
+  getStaticProductImage( imageName: string ) {
 
-        const path = join( __dirname, '../../static/products', imageName );
+    const path = join( __dirname, '../../static/products', imageName );
 
-        if ( !existsSync(path) ) 
-            throw new BadRequestException(`No product found with image ${ imageName }`);
+    if ( !existsSync(path) ) 
+        throw new BadRequestException(`No product found with image ${ imageName }`);
 
-        return path;
-    }
-
-
+    return path;
+  }
 }
