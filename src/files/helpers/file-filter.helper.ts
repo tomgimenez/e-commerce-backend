76746c5fe03a2f8c3dad64b7ff226b4b89a2c999
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-05-29 12:30:07 -0300

    change file name and delete another one that was unused

--- src/files/helpers/file-filter.helper.ts
+++ src/files/helpers/file-filter.helper.ts
@@ -1,5 +1,3 @@
-
-
 export const fileFilter = ( req: Express.Request, file: Express.Multer.File, callback: Function ) => {
 
     if ( !file ) return callback( new Error('File is empty'), false );
@@ -11,6 +9,5 @@ export const fileFilter = ( req: Express.Request, file: Express.Multer.File, cal
         return callback( null, true )
     }
 
-    callback(null, false );
-
+    callback(null, false);
 }
