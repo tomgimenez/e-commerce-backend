5ef4939874eb265020c4e8b457cd2267630da07a
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-06-12 18:14:40 -0300

    remove unneeded import

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
 
 import { NotificationsGateway } from './notifications.gateway';
 import { NotificationsService } from './notifications.service';
