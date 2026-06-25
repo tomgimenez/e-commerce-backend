33823f2aa8951ffa23017c7a9950ec34e55c595e
Tomás Giménez <tomasgimenez86@gmail.com>
2026-06-25 19:46:11 -0300

    add config to parse value correctly

--- src/shipping/entities/shipping-method.entity.ts
+++ src/shipping/entities/shipping-method.entity.ts
@@ -12,7 +12,10 @@ export class ShippingMethod {
   @Column({ type: 'text', nullable: true })
   description: string;
 
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  @Column({ type: 'decimal', precision: 10, scale: 2, transformer: {
+    to: (value: number) => value,
+    from: (value: string) => Number.parseFloat(value),
+  } })
   price: number;
 
   @Column({ default: true })
