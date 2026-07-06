07ed64fa2258f58012f92ed593ce9ff721bdfce0
Tomás Giménez <tomasgimenez86@gmail.com>
2026-07-06 18:36:56 -0300

    update module dependencies

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -5,12 +5,19 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
+import { MercadoPagoProvider } from './mercadopago.provider';
+import { Cart } from 'src/cart/entities/cart.entity';
+import { Address } from 'src/address/entities/address.entity';
+import { ShippingMethod } from 'src/shipping/entities/shipping-method.entity';
+import { Tax } from 'src/tax/entities/tax.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [OrderService],
+  providers: [OrderService, MercadoPagoProvider],
   controllers: [OrderController],
   imports: [
-    TypeOrmModule.forFeature([ Order, OrderItem ])
+    TypeOrmModule.forFeature([ Order, OrderItem, Cart, Address, ShippingMethod, Tax ]),
+    ConfigModule
   ]
 })
 export class OrderModule {}
